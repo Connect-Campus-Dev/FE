f790da5a4e89637d573a1c8bcb554fc69437dccd
moon <88218891+man-moon@users.noreply.github.com>
2023-08-07 02:46:23 +0900

    [핫픽스] 서버 주소 변경

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,4 +1,4 @@
 import { writable } from 'svelte/store';
 
 export const toastMessage = writable(null);
-export const API_BASE_URL = 'http://222.251.154.231:8080';
+export const API_BASE_URL = 'https://222.251.154.231:8080';
