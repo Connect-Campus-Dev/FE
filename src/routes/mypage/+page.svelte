98cc2441fcaf8917b60bf56cbb839691edc82d6d
moon <88218891+man-moon@users.noreply.github.com>
2023-09-13 18:40:25 +0900

    [수정] 미사용 컴포넌트 제거

--- src/routes/mypage/+page.svelte
+++ src/routes/mypage/+page.svelte
@@ -90,7 +90,7 @@
     <hr class="h-2 bg-gray-100" />
 
     <!-- 사진첩 -->
-    <div class="flex items-center justify-between p-4 mt-4">
+    <!-- <div class="flex items-center justify-between p-4 mt-4">
         <h3>
             <div class="font-bold text-lg text-gray-700">내 사진첩</div>
             <div class="text-sm text-gray-500">
@@ -101,8 +101,8 @@
             더 보기
             <Icon icon="chevron-right" size={16} />
         </button>
-    </div>
-    <div class="px-4 mb-4">
+    </div> -->
+    <!-- <div class="px-4 mb-4">
         <div class="overflow-x-auto flex gap-4">
           {#each "0123456789" as tag}
             <div>
@@ -112,12 +112,12 @@
         </div>
       </div>
 
-    <hr class="my-4 h-2 bg-gray-100" />
+    <hr class="my-4 h-2 bg-gray-100" /> -->
 
-    <!-- 내가 쓴 포스트 -->
+    <!-- 내가 쓴 게시글 -->
     <div class="flex items-center justify-between p-4">
         <h3>
-            <div class="font-bold text-lg text-gray-700">내가 쓴 포스트</div>
+            <div class="font-bold text-lg text-gray-700">내가 쓴 게시글</div>
         </h3>
         <button class="text-sm text-gray-600 flex items-center">
             더 보기
@@ -129,10 +129,10 @@
             <div class="flex justify-between gap-4 py-4 {i != 3 ? 'border-b' : ''}">
                 <div class="flex flex-col">
                     <div class="font-semibold text-lg text-gray-700">
-                        포스트 제목
+                        게시글 제목
                     </div>
                     <div class="text-sm text-gray-500">
-                        포스트 내용
+                        게시글 내용
                     </div>
                 </div>
                 <div class="bg-gray-300 w-24 h-24 rounded-lg" />
